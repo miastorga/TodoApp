fa6a54a99286a1ec142f7f325574d1be8e9d13ad
Miguel Astorga <mastorga.leiva@gmail.com>
2022-11-13 21:35:02 -0300

    header borrado

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,7 +1,6 @@
 import { useAutoAnimate } from '@formkit/auto-animate/react'
 import { useContext } from 'react'
 import { Context } from '/src/context/TodoContext'
-import { ListHeader } from './ListHeader'
 import { Todo } from './Todo'
 export const TodoList = () => {
 	const [parent] = useAutoAnimate()
@@ -9,7 +8,6 @@ export const TodoList = () => {
 
 	return (
 		<ul ref={parent}>
-			<ListHeader />
 			{todoItems.map((todo) => (
 				<Todo key={todo.id} todo={todo} />
 			))}
